2f7429dfa1d91fe0a30ea407d5dde1e4516e6555
Krishanu <krishanu.info@gmail.com>
2022-07-05 21:56:44 +0530

    clean up previous

--- resources/views/brand/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<p class="small m-n">
-    © Copyright {{date('Y')}} <a href="{{env('APP_URL')}}" target="_blank">{{env('APP_NAME')}}</a>
-</p>
--- resources/views/optional/setup/installer.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.webtube.front.master')
-@section('content')
-
-    <h1> Installation Window</h1>
-
-@endsection
